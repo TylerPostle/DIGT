ae0861b572af5f0b07eb09bf94bd4e0cc51354a9
TylerPostle <158524571+TylerPostle@users.noreply.github.com>
2024-04-05 12:50:15 +1300

    Create assessment.ino

---
+++ assessment.ino
@@ -0,0 +1,30 @@
+#include <Wire.h>
+#include "SparkFun_Qwiic_OpenLog_Arduino_Library.h"
+#include <SparkFun_LPS25HB_Arduino_Library.h> //Click here to get the library: http://librarymanager/All#SparkFun_LPS25HB
+
+LPS25HB pressureSensor; // Create an object of the LPS25HB class
+OpenLog TylersLog; //Create instance
+
+void setup()
+{
+
+  Wire.begin();
+  TylersLog.begin(); //Open connection to OpenLog (no pun intended)
+pressureSensor.begin()
+  Serial.begin(9600); //9600bps is used for debug statements
+
+  TylersLog.append("TylersLog.txt");
+  TylersLog.println("testing");
+ TylersLog.syncFile();
+
+  Serial.println("Done!");
+}
+
+void loop() {
+    Serial.print("Pressure in hPa: ");
+  Serial.print(pressureSensor.getPressure_hPa()); // Get the pressure reading in hPa
+  Serial.print(", Temperature (degC): ");
+  Serial.println(pressureSensor.getTemperature_degC()); // Get the temperature in degrees C
+
+  delay(40); // Wait - 40 ms corresponds to the maximum update rate of the sensor (25 Hz)
+}
